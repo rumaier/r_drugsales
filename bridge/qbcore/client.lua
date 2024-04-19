7fe3863a5882d211ffdc01b4cadd3470a094c818
rumaier <148654629+rumaier@users.noreply.github.com>
2024-04-19 10:39:24 -0500

    fix QB notification type error

--- bridge/qbcore/client.lua
+++ bridge/qbcore/client.lua
@@ -4,7 +4,7 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 function ClNotify(msg, type)
     if Cfg.Notification == 'default' then
-        TriggerEvent('QBCore:Notify', msg, type, 3000)
+        TriggerEvent('QBCore:Notify', msg, 'primary', 3000)
     elseif Cfg.Notification == 'ox' then
         lib.notify({ description = msg, type = type, position = 'top' })
     elseif Cfg.Notification == 'custom' then
--- bridge/qbcore/server.lua
+++ bridge/qbcore/server.lua
@@ -6,7 +6,7 @@ local QBCore = exports['qb-core']:GetCoreObject()
 function SvNotify(msg, type)
     local src = source
     if Cfg.Notification == 'default' then
-        TriggerClientEvent('QBCore:Notify', src, msg, type)
+        TriggerClientEvent('QBCore:Notify', src, msg, 'primary')
     elseif Cfg.Notification == 'ox' then
         TriggerClientEvent('ox_lib:notify', src, { description = msg, type = type, position = 'top' })
     elseif Cfg.Notification == 'custom' then
