6e251177e8a7b5e2e0af5b027ce806c930a6c961
rumaier <148654629+rumaier@users.noreply.github.com>
2026-03-30 17:20:41 -0500

    chore: bump manifest version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,25 +3,24 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-name 'resource_name'
-description 'resource_description'
-author 'author_name'
-version '1.0.0'
+name 'r_drugsales'
+description 'A Simple Drug Selling Script'
+author 'rumaier'
+version '3.2.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
     'core/shared/*.lua',
     'locales/*.lua',
-    'config.lua',
+    'config.lua'
 }
 
 server_scripts {
-    '@oxmysql/lib/MySQL.lua', -- Uncomment if resource uses oxmysql
-    'core/server/*.lua',
+    'core/server/*.lua'
 }
 
 client_scripts {
-    'core/client/*.lua',
+    'core/client/*.lua'
 }
 
 ui_page 'web/dist/index.html'
@@ -35,8 +34,7 @@ dependencies {
 }
 
 escrow_ignore {
-    'core/server/logging.lua',
     'install/**/*.*',
     'locales/*.*',
-    'config.lua'    
+    'config.lua'
 }
